4036b61a964510bf3b9cf0a2bbd0682e3444449c
Yi Yang <tanyiyang2032@gmail.com>
2023-05-31 18:36:57 +0800

    feat: blackjackcontrols

--- src/components/games/blackjack/Controls.tsx
+++ src/components/games/blackjack/Controls.tsx
@@ -1,13 +1,40 @@
 'use client';
 
+import { Button } from "@/components/ui/Button";
+import { TbPokerChip } from "react-icons/tb";
+import { TbCoins } from "react-icons/tb";
 interface ControlsProps {
 }
 
 export function Controls ({
 } ) {
     return (
-      <div>
+      <div className="
+      grid
+      grid-rows-2 
+      h-screen
+      justify-center
+      ">
+        <div className="grid row-span-1 grid-cols-2 h-[50vh] items-center">
+          <div className="grid grid-rows-2 text-center justify-center">
+          <TbPokerChip size={100} />
+          50
+          </div>
+          <div className="grid grid-rows-2 text-center justify-center">
+          <TbCoins size={100} />
+          250
+          </div>
+          
+          
+        </div>
 
+        <div className="grid row-span-1 grid-cols-2 h-[10vh] gap-5">
+          <div><Button label={"Hit"}></Button></div>
+          <div><Button label={"Double"}></Button></div>
+          <div><Button label={"Stand"}></Button></div>
+          <div><Button label={"Split"}></Button></div>
+        </div>
+        
       </div>
     )
 }
